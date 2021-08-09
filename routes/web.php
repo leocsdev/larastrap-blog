81f38d4cfb62af9b3e80179617bf248687ebbf1c
Leo <leocsdev@gmail.com>
2021-08-10 00:29:30 +0800

    Fixed create route issue.

--- routes/web.php
+++ routes/web.php
@@ -5,34 +5,27 @@ use App\Http\Controllers\PostController;
 
 Auth::routes();
 
-// PUBLIC ROUTES
-
-// Redirected to show all posts, 
+// Redirects
 Route::redirect('/', '/posts');
+Route::redirect('/home', '/posts');
 
 // Show all posts
 Route::get('/posts', [PostController::class, 'index']);
 
+// Add a post, show form
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth');
+
+// Save post
+Route::post('/posts', [PostController::class, 'store'])->middleware('auth');
+
 // Show single post
 Route::get('/posts/{post}', [PostController::class, 'show']);
 
+// Edit post
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->middleware('auth');
 
+// Update post
+Route::put('/posts/{post}', [PostController::class, 'update'])->middleware('auth');
 
-// PROTECTED ROUTES
-
-Route::prefix('/posts')->middleware('auth')->group( function  () {
-    // Add a post, show form
-    Route::get('/create', [PostController::class, 'create']);
-
-    // Save post
-    Route::post('/', [PostController::class, 'store']);
-
-    // Edit post
-    Route::get('/{post}/edit', [PostController::class, 'edit']);
-
-    // Update post
-    Route::put('/{post}', [PostController::class, 'update']);
-
-    // Delete post
-    Route::delete('/{post}', [PostController::class, 'destroy']);
-});
+// Delete post
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->middleware('auth');
