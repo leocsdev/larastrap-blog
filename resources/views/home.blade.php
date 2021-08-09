9717d6c789d208e4539602f1ee6cae72647e55a4
Leo <leocsdev@gmail.com>
2021-08-09 13:43:12 +0800

    Show all posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        @if (count($posts) == 0)
+            <h3>
+                No blog posts to show, please
+                <a href="/posts/create">
+                    <span>add a post</span>
+                </a>
+            </h3>
+        @else
+            <h2 class="text-center my-5">Blog Posts</h2>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <div class="row justify-content-center">
+                @foreach ($posts as $post)
+                    <div class="col-md-6 col-lg-4 p-3">
+                        <div class="card h-100">
+                            <img class="card-img-top" src="{{ $post->cover_image }}" alt="{{ $post->title }}">
+                            <div class="card-body">
+                                <div class="card-title">
+                                    <h3 class="text-center">{{ $post->title }}</h3>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                    </div>
+                @endforeach
             </div>
-        </div>
+        @endif
+
+
+
+
+
     </div>
-</div>
 @endsection
