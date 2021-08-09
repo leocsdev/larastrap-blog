99650554ef7a0735fcad024ef49ccc5b957969da
Leo <leocsdev@gmail.com>
2021-08-10 01:18:23 +0800

    Created has many relationship with posts.

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // one user has many posts
+    function posts() {
+        return $this->hasMany('App\Models\Post');
+    }
 }
