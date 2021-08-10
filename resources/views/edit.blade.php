e4fa33489c33c538d16a80a87a18042136bee8e4
Leo <leocsdev@gmail.com>
2021-08-10 19:07:07 +0800

    Added security to prevent logged in user to edit someone else's post.

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,45 +4,53 @@
     <section class="container">
         <h2 class="text-center my-4">Edit Blog Post</h2>
 
-        <form action="/posts/{{ $post->id }}" method="post">
-            @csrf
-            @method('PUT')
+        @if (Auth::check() && Auth::user()->name == $post->user->name)
+            <form action="/posts/{{ $post->id }}" method="post">
+                @csrf
+                @method('PUT')
 
-            @if ($errors->any())
-                <ul class="alert alert-danger">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                @if ($errors->any())
+                    <ul class="alert alert-danger">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
 
-                    @foreach ($errors->all() as $error)
-                        <li class="mx-4">{{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
+                        @foreach ($errors->all() as $error)
+                            <li class="mx-4">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
 
-            <div class="form-group">
-                <label for="">Title</label>
-                <input type="text" name="title" class="form-control" value="{{ $post->title }}">
+                <div class="form-group">
+                    <label for="">Title</label>
+                    <input type="text" name="title" class="form-control" value="{{ $post->title }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="">Cover Image Link</label>
+                    <input type="text" name="cover_image" class="form-control" value="{{ $post->cover_image }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="body">Content</label>
+                    <textarea name="body" class="form-control" rows="20">{{ $post->body }}</textarea>
+                </div>
+
+                <input type="text" name="user_id" hidden readonly value="{{ Auth::user()->id }}">
+
+                <button type="submit" class="btn btn-primary">Update</button>
+
+                <a href="{{ url()->previous() }}" class="btn btn-secondary">
+                    Cancel
+                </a>
+            </form>
+        @else
+            <h3 class="text-center py-4">You're not the owner of this post, you don't have permission to edit.</h3>
+
+            <div class="text-center">
+                <a href="{{ url()->previous() }}" class="btn btn-secondary">Go back</a>
             </div>
-
-            <div class="form-group">
-                <label for="">Cover Image Link</label>
-                <input type="text" name="cover_image" class="form-control" value="{{ $post->cover_image }}">
-            </div>
-
-            <div class="form-group">
-                <label for="body">Content</label>
-                <textarea name="body" class="form-control" rows="20">{{ $post->body }}</textarea>
-            </div>
-
-            <input type="text" name="user_id" hidden readonly value="{{ Auth::user()->id }}">
-
-            <button type="submit" class="btn btn-primary">Update</button>
-
-            <a href="{{ url()->previous() }}" class="btn btn-secondary">
-                Cancel
-            </a>
-        </form>
+        @endif
     </section>
 
     <script src="https://cdn.tiny.cloud/1/loe8d7t7rt33e92q3z27xoiibdqk1o9tbwmz4qgynq99kghw/tinymce/5/tinymce.min.js"
